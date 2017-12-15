a8bc74a0105c2ac0e258e01a41503f2074248a1a
Jared Roesch <roeschinc@gmail.com>
2017-12-15 00:28:40 -0800

    Add skeleton for abstract maps

---
+++ lean/src/puddle/map.lean
@@ -0,0 +1,27 @@
+namespace puddle
+
+def map (k v : Type) := list (k × v)
+
+namespace map
+
+def empty {k v : Type} : map k v :=
+@list.nil (k × v)
+
+def lookup {k v : Type} : map k v → k → option v
+| cx k := none
+
+def insert {k v : Type} : map k v → k → v → map k v
+| cx k v := cx
+
+def remove {k v : Type} : map k v → k → map k v
+| cx k := cx
+
+def extend {k v : Type} : map k v → map k v → map k v
+| cx cx' := cx
+
+def shrink {k v : Type} : map k v → list k → map k v
+| cx keys := cx
+
+end map
+
+end puddle
